c853d8131bea7c458bc9be4d70508fa6a4498203
wilfriedE <wilfried.hounyo@gmail.com>
2014-07-28 21:40:33 -0700

    initial commit

---
+++ themer.rb
@@ -0,0 +1,23 @@
+case ARGV[0]
+
+	when 'bashify'
+	 system "prompt %USERNAME%@%COMPUTERNAME%:~$$$S"
+	 system "color 2"
+	when 'deep-blue'
+	 system "color 1a"
+	when 'alien'
+	 system "color 2b"
+	when 'lime'
+	 system "color 2e"
+	when 'lime'
+	 system "color 2e"
+	when 'lightblue'
+	 system "color 3a"
+	when 'babyblue'
+	 system "color 3b"
+	when 'pinksky'
+	 system "color 3d"
+	when 'vomit'
+	 system "color 4a"
+end
+
